98c8ffeba0faa9d564ef8e94632eecd502e598ba
agonzale34 <agonzale34@gmail.com>
2019-01-27 12:40:49 -0500

    other test

--- model.py
+++ model.py
@@ -2,17 +2,19 @@ import csv
 
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout, BatchNormalization, Activation
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout, BatchNormalization, Activation, \
+    MaxPooling2D
 from keras.models import Sequential
+from keras.optimizers import Adam
 
 lines = []
 angle_correction = 0.2
 data_folder = './training-data/'
 
 
-def add_image_list(img_list, in_img_path):
-    img_name_path = data_folder + 'IMG/' + in_img_path.split('/')[-1]
-    img_list.append(plt.imread(img_name_path))
+def open_image(in_path):
+    img_name_path = data_folder + 'IMG/' + in_path.split('/')[-1]
+    return plt.imread(img_name_path)
 
 
 # read all the lines in the csv file
@@ -32,13 +34,16 @@ for i in range(0, len(lines), 2):
     angle_left = angle_center + angle_correction
     angle_right = angle_center - angle_correction
     angles = angles + [angle_center, angle_left, angle_right]
-    add_image_list(images, lines[i][0])
-    add_image_list(images, lines[i][1])
-    add_image_list(images, lines[i][2])
+    image_center = open_image(lines[i][0])
+    image_left = open_image(lines[i][1])
+    image_right = open_image(lines[i][2])
+    images.append(image_center)
+    images.append(image_left)
+    images.append(image_right)
     # data augmentation
-    images.append(np.fliplr(plt.imread(lines[i][0])))
-    images.append(np.fliplr(plt.imread(lines[i][1])))
-    images.append(np.fliplr(plt.imread(lines[i][2])))
+    images.append(np.fliplr(image_center))
+    images.append(np.fliplr(image_left))
+    images.append(np.fliplr(image_right))
     angles.append(-angle_center)
     angles.append(-angle_left)
     angles.append(-angle_right)
@@ -49,25 +54,29 @@ y_train = np.array(angles)
 print('Dataset loaded successfully -> features:', len(angles))
 
 RELU = 'relu'
+SAME = 'same'
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation=RELU))
-model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation=RELU))
-# model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation=RELU))
-model.add(Conv2D(64, kernel_size=(3, 3), activation=RELU))
-# model.add(Conv2D(64, kernel_size=(3, 3), activation=RELU))
+model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), padding=SAME, activation=RELU))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), padding=SAME, activation=RELU))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+# model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), padding=SAME, activation=RELU))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Conv2D(64, kernel_size=(3, 3), padding=SAME, activation=RELU))
+# model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Conv2D(64, kernel_size=(3, 3), padding=SAME, activation=RELU))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dropout(0.5))
 model.add(Dense(50))
-model.add(Dropout(0.5))
 model.add(Dense(10))
-model.add(Dropout(0.5))
 model.add(Dense(1))
 print('DriverNet created successfully')
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer=Adam(0.0001))
 history_object = model.fit(X_train, y_train, validation_split=0.3, shuffle=True, epochs=7)
 
 # Save the model
