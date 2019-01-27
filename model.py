e223ae7b49de4d6dedd42e7c8200b90586c5083a
agonzale34 <agonzale34@gmail.com>
2019-01-27 16:35:55 -0500

    10 epochs train

--- model.py
+++ model.py
@@ -2,8 +2,7 @@ import csv
 
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout, BatchNormalization, Activation, \
-    MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 
@@ -17,7 +16,7 @@ def open_image(in_path):
     return plt.imread(img_name_path)
 
 
-ACTI = 'elu'
+ACTI = 'relu'
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
@@ -59,11 +58,11 @@ for i in range(0, len(lines), 1):
     images.append(image_right)
     # data augmentation
     images.append(np.fliplr(image_center))
-    images.append(np.fliplr(image_left))
-    images.append(np.fliplr(image_right))
+    # images.append(np.fliplr(image_left))
+    # images.append(np.fliplr(image_right))
     angles.append(-angle_center)
-    angles.append(-angle_left)
-    angles.append(-angle_right)
+    # angles.append(-angle_left)
+    # angles.append(-angle_right)
 
 # creating the dataset for training the model
 X_train = np.array(images)
@@ -76,15 +75,3 @@ history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True,
 # Save the model
 model.save('model.h5')
 print('DriverNet trained and saved successfully')
-
-# print the keys contained in the history object
-print(history_object.history.keys())
-
-# plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
